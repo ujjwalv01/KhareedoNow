2828e8b63a2ac7c15a3c1e6f213889f34f1adf00
Ujjwal Verma <ujjwalverma010305@gmail.com>
2026-06-23 23:04:49 +0530

    commmittttt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,7 +12,7 @@ const registerUser = async (req, res) => {
         // TODOS: Hash the password before saving to DB
         // TODOS: Implment JWT tokens for authentication and authorization
         // TODOS: OTP send & verification
-        // TODOS: welcome mail to the user
+        // TODOS: welcome mail to the user 
         const user = await User.create({ name, email, password, role });
         res.status(201).json({ message: "User created successfully", user });
     } catch (error) {
