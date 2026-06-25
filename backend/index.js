1f2d48446e6a321960e70936002f592cfddd38f8
Ujjwal Verma <ujjwalverma010305@gmail.com>
2026-06-25 20:39:14 +0530

    feat: implement product management routes and controllers with expanded API endpoints

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,11 @@ app.get("/", (req, res) => {
     res.send("KhareedoNow Backend is working");
 });
 
-app.use('api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
+app.use('/api/analytics', require('./routes/analyticRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { protect } = require('../middleware/authMiddleware');
+const admin = require('../middleware/adminMiddleware');
+const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+
+const router = express.Router();
+
+
+router.post('/register', registerUser);
+router.get('/register', registerUser);
+
+// For all products
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+
+// For individual products
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+
+
+module.exports = router;
